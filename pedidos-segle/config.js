c10353be0e62ccb7edd4fd18b6e16214b893604f
carlopr27 <carlo.pontecorvo.rodriguez@gmail.com>
2026-09-10 17:15:17 -0600

    Update apiUrl in config.js

--- pedidos-segle/config.js
+++ pedidos-segle/config.js
@@ -1,4 +1,4 @@
 window.SEGLE_CONFIG = {
-  apiUrl: "", // Pega aquí la URL de Apps Script terminada en /exec
+  apiUrl: "https://script.google.com/macros/library/d/19PD1D2B2JE_1Km756_icFXhWd5LV_R7bQF6CZLPHX7HgpceyBpxiL8Rp/7", // Pega aquí la URL de Apps Script terminada en /exec
   mode: "connected"
 };
