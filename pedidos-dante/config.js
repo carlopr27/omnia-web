1af36c300fa9436f973bfad5706a93e2b482a44f
carlopr27 <carlo.pontecorvo.rodriguez@gmail.com>
2026-09-16 16:17:34 -0600

    Update apiUrl in config.js

--- pedidos-dante/config.js
+++ pedidos-dante/config.js
@@ -1,4 +1,4 @@
 window.DANTE_CONFIG = {
-  apiUrl: "", // Pega aquí la URL de Apps Script terminada en /exec
+  apiUrl: "https://script.google.com/macros/s/AKfycbwhHpbfF3ofJV27-kKHkVE-FPR62vOlmPoD5vTvEwl6uPP_-O_qyHudBMgOf1s7lLmH/exec", // Pega aquí la URL de Apps Script terminada en /exec
   mode: "connected"
 };
